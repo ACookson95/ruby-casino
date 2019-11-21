3dafd4c0d938aa1ea5a4157e7094b459f5991b8e
Gabriella Algecera <gabriellaalgecera@Brians-MacBook-Pro.local>
2019-11-20 19:10:41 -0700

    fix slots menu

--- slots.rb
+++ slots.rb
@@ -1,79 +1,56 @@
+
+
 @slots_symbols = [ "cherry", "-bells", "--bar-", "-seven" ]
+@play_cost = 1
+@jackpot_win = 1000
+@seven_win = 70
 
 def slots
+puts "\n=======================================================================".colorize(:yellow)
 puts "WELCOME TO"
-puts "HOT SLOTS"
+puts "HOT SHOTS-SLOTS".colorize(:red)
 puts "\n"
-puts "1) Pull the lever (-$1)"
-puts "2) Go back to the game menu"
-puts "3) Check wallet"
-puts "\n"
+puts "1) Pull the lever (-$1)".colorize(:cyan)
+puts "2) Go back to the game menu".colorize(:cyan)
+puts "3) Check wallet".colorize(:green)
+puts "\n=======================================================================".colorize(:yellow)
 menu_choice = gets.strip.to_i
 case menu_choice
 when 1
+    @user.wallet -= @play_cost
     machine_calc
-when menu_choice == 2
+when 2
     game_menu
-when menu_choice == 3
-    puts "Your current balance is #{@user.wallet}"
+when 3
+    puts "Your current balance is #{@user.wallet}".colorize(:green)
     slots
 else 
     puts "\n"
-    puts "Not an option, buddy"
+    puts "Not an option, hot-shot".colorize(:red)
     puts "\n"
     slots
 end
 end
-            #                   ".-------."
-            #                   "|Jackpot|"
-            #       "____________|_______|____________"
-            #      "|  __    __    ___  _____   __    |"  
-            #      "| / _\  / /   /___\/__   \ / _\   |" 
-            #      "| \ \  / /   //  //  / /\ \\ \  25|" 
-            #      "| _\ \/ /___/ \_//  / /  \/_\ \ []|" 
-            #      "| \__/\____/\___/   \/     \__/ []|"
-            #      "|===_______===_______===_______===|"
-            #      "||*|\_     |*| _____ |*|\_     |*||"
-            #      "||*|| \ _  |*||     ||*|| \ _  |*||"
-            #      "||*| \_(_) |*||*BAR*||*| \_(_) |*||"
-            #      "||*| (_)   |*||_____||*| (_)   |*|| __"
-            #      "||*|_______|*|_______|*|_______|*||(__)"
-            #      "|===_______===_______===_______===| ||"
-            #      "||*| _____ |*|\_     |*|  ___  |*|| ||"
-            #      ||*||     ||*|| \ _  |*| |_  | |*|| ||
-            #      ||*||*BAR*||*| \_(_) |*|  / /  |*|| ||
-            #      ||*||_____||*| (_)   |*| /_/   |*|| ||
-            #      ||*|_______|*|_______|*|_______|*||_//
-            #      |===_______===_______===_______===|_/
-            #      ||*|  ___  |*|   |   |*| _____ |*||
-            #      ||*| |_  | |*|  / \  |*||     ||*||
-            #      ||*|  / /  |*| /_ _\ |*||*BAR*||*||              
-            #      ||*| /_/   |*|   O   |*||_____||*||        
-            #      ||*|_______|*|_______|*|_______|*||
-            #      |lc=___________________________===|
-            #      |  /___________________________\  |
-            #      |   |                         |   |
-            #     _|    \_______________________/    |_
-            #    (_____________________________________)
 
 def machine_calc
-    # @user.wallet -= 1
-
     @slot_1 = @slots_symbols.shuffle.first
     @slot_2 = @slots_symbols.shuffle.first
     @slot_3 = @slots_symbols.shuffle.first
-    puts "\n"
-    puts "\n"
-    puts "|====_________====|"
-    puts "||*|           |*||"
-    puts "||*|--#{@slot_1}---|*|| (_)"
-    puts "||===_________===||  |"
-    puts "||*|           |*||  |"
-    puts "||*|--#{@slot_2}---|*||_/"
-    puts "||===_________===||"
-    puts "||*|           |*||"
-    puts "||*|--#{@slot_3}---|*||"
-    puts "||*|___________|*||"
+    puts"\n"
+    puts"\n"
+    puts"      .-------.".colorize(:yellow)
+    puts"      |HotShot|".colorize(:red)
+    puts" |====_________====|".colorize(:cyan)
+    puts" ||*|           |*||".colorize(:cyan)
+    puts" ||*|--#{@slot_1}---|*|| (_)".colorize(:cyan)
+    puts" ||===_________===||  |".colorize(:cyan)
+    puts" ||*|           |*||  |".colorize(:cyan)
+    puts" ||*|--#{@slot_2}---|*||_/".colorize(:cyan)
+    puts" ||===_________===||".colorize(:cyan)
+    puts" ||*|           |*||".colorize(:cyan)
+    puts" ||*|--#{@slot_3}---|*||".colorize(:cyan)
+    puts" ||*|___________|*||".colorize(:cyan)
+    puts"(___________________)".colorize(:yellow)
     puts "\n"
     puts "\n"
 
@@ -81,12 +58,12 @@ def machine_calc
         winnings
     elsif @slot_1 == @slot_2 || @slot_3 == @slot_1 || @slot_2 == @slot_3
         puts "\n"
-        puts "So close!"
+        puts "So close!".colorize(:red)
         puts "\n"
         slots
     else
         puts "\n"
-        puts "Next time!"
+        puts "Next time!".colorize(:red)
         puts "\n"
         slots
     end
@@ -94,18 +71,55 @@ def machine_calc
 
 def winnings
     if @slot_1 == "--bar-"
-         puts "You won a free drink."    
-         slots
+        puts "You won a free drink.".colorize(:red)
+        puts "\n" 
+        slots
     elsif @slot_1 == "cherry"
-        puts "You won a free meal!"
+        puts "You won a free meal!".colorize(:red)
+        puts "\n"
         slots
     elsif @slot_1 == "-seven"
-        puts "You win seventy dollars!"
-        # @user.wallet += 70
+        puts "You win seventy dollars!".colorize(:red)
+        puts "\n"
+        @user.wallet += @seven_win
         slots
     elsif @slot_1 == "-bells"
-        puts "Jackpot!"
-        # @user.wallet += 1000
+        puts"\n"
+        puts"                              .-------.".colorize(:yellow)
+        puts"                              |HotShot|".colorize(:red)
+        puts"                  ____________|_______|____________".colorize(:yellow)
+        puts"                 |    __  __    ___ ______   ___   |".colorize(:yellow)  
+        puts"                 |  / __|/ /   /___|__   _|/ ___|  |".colorize(:yellow) 
+        puts"                 |  | | / /   //  // / /  | |    $1|".colorize(:yellow) 
+        puts"                 | _' |/ /___/ |_// / / __/ /    |||".colorize(:yellow)
+        puts"                 ||__/|____//___// /_/ |___/     |||".colorize(:yellow)
+        puts"                 |===_______===_______===_______===|".colorize(:yellow)
+        puts"                 ||*||_     |*| _____ |*||_     |*||".colorize(:cyan)
+        puts"                 ||*|| | _  |*||     ||*|| | _  |*||".colorize(:cyan)
+        puts"                 ||*| '_(_) |*||*BAR*||*| '_(_) |*||".colorize(:cyan)
+        puts"                 ||*| (_)   |*||_____||*| (_)   |*|| __".colorize(:cyan)
+        puts"                 ||*|_______|*|_______|*|_______|*||(__)".colorize(:cyan)
+        puts"                 |===_______===_______===_______===| ||".colorize(:cyan)
+        puts"                 ||*| _____ |*||_     |*|  ___  |*|| ||".colorize(:cyan)
+        puts"                 ||*||     ||*|| | _  |*| |_  | |*|| ||".colorize(:cyan)
+        puts"                 ||*||*BAR*||*| '_(_) |*|  / /  |*|| ||".colorize(:cyan)
+        puts"                 ||*||_____||*| (_)   |*| /_/   |*|| ||".colorize(:cyan)
+        puts"                 ||*|_______|*|_______|*|_______|*||_//".colorize(:cyan)
+        puts"                 |===_______===_______===_______===|_/".colorize(:cyan)
+        puts"                 ||*|  ___  |*|   |   |*| _____ |*||".colorize(:cyan)
+        puts"                 ||*| |_  | |*|  / '  |*||     ||*||".colorize(:cyan)
+        puts"                 ||*|  / /  |*| /_ _' |*||*BAR*||*||".colorize(:cyan)             
+        puts"                 ||*| /_/   |*|   O   |*||_____||*||".colorize(:cyan)       
+        puts"                 ||*|_______|*|_______|*|_______|*||".colorize(:cyan)
+        puts"                 |lc=___________________________===|".colorize(:yellow)
+        puts"                 |  |___________________________|  |".colorize(:yellow)
+        puts"                 |   |                         |   |".colorize(:yellow)
+        puts"                _|    |_______________________|    |_".colorize(:yellow)
+        puts"               (_____________________________________)".colorize(:yellow)
+        puts"\n"
+        puts"                       You won the Jackpot!!!".colorize(:red)
+        puts"\n"
+        @user.wallet += @jackpot_win
         slots
     else
         slots
