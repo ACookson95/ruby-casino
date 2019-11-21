2b868d622467c0f060112ccec67895e1572c1b62
Aaron Cookson <cookson.aaron@gmail.com>
2019-11-21 01:39:42 -0700

    created a blackjack game

--- ruby_casino.rb
+++ ruby_casino.rb
@@ -26,7 +26,7 @@
 
 require 'pry'
 require 'colorize'
-# require_relative 'Blackjack'
+require_relative 'blackjack'
 require_relative 'high_low'
 require_relative 'slots'
 
@@ -118,7 +118,7 @@ end
 def game_menu
   puts "\n=======================================================================".colorize(:red)
   puts "\nGame Menu:"
-  puts "1: Black Jack"
+  puts "1: Blackjack"
   puts "2: Slots"
   puts "3: High / Low"
   puts "4: Main Menu"
